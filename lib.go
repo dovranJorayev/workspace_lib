21d17804df841723bada64e6b502294a7ec3b37e
dovran <sony.energizer@gmail.com>
2025-10-07 10:35:55 +0500

    Refactor comments for clarity in AddNums and SubNums functions

--- lib.go
+++ lib.go
@@ -1,9 +1,12 @@
+// Package workspace_lib provides essential (mock) features to work related apps.
 package workspace_lib
 
+// AddNums returns sum of two finite numbers.
 func AddNums(a, b int) int {
 	return a + b
 }
 
+// SubNums subtracts two finite numbers and returns result.
 func SubNums(a, b int) int {
 	return a - b
 }
