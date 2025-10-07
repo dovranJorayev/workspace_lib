be3c4d25063985e596f5bc3a2624d1ae40237422
dovran <sony.energizer@gmail.com>
2025-10-07 11:29:17 +0500

    Refactor AddNums function to support generic number types and create v1 module structure

--- v1/lib.go
+++ v1/lib.go
@@ -1,15 +1,9 @@
 // Package workspace_lib provides essential (mock) features to work related apps.
 package workspace_lib
 
-import "golang.org/x/exp/constraints"
-
-type Number interface {
-	constraints.Integer | constraints.Float
-}
-
 // AddNums returns sum of two finite numbers.
 // https://www.mathsisfun.com/numbers/addition.html
-func AddNums[N Number](a, b N) N {
+func AddNums(a, b int) int {
 	return a + b
 }
 
